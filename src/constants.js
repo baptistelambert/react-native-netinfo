865dfb311e90452d803eba7852377b22d61ce59c
baptistelambert <bapthf2@gmail.com>
2018-02-15 20:20:37 +0100

    add connectionInfo constants

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+const CONSTANTS = {
+  CONNECTION_INFO: {
+    TYPES: {
+      NONE: 'none',
+      WIFI: 'wifi',
+      CELLULAR: 'cellular',
+      UNKNOWN: 'unknown',
+      BLUETOOTH: 'bluetooth',
+      ETHERNET: 'ethernet',
+      WIMAX: 'wimax'
+    },
+    EFFECTIVE_TYPES: {
+      '2G': '2g',
+      '3G': '3g',
+      '4G': '4g',
+      UNKNOWN: 'unknown'
+    }
+  }
+};
+
+export { CONSTANTS };
